a2b19ab5656c5baa66596887eb207a1c17a59dc7
rinto11804 <rinto11804@gmail.com>
2024-10-17 22:23:25 +0530

    basic dashboard for staff

--- app.py
+++ app.py
@@ -17,3 +17,8 @@ def signup():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
