affd0487154f7518a53c20a55714e69528661a33
rinto11804 <rinto11804@gmail.com>
2024-10-21 06:42:34 +0530

    database connection added

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from flask import render_template, request
+from dotenv import load_dotenv
+from flask_mysqldb import MySQL
+
+# import MySQLdb.cursors
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.secret_key = "a_secret_key"
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+
+mysql = MySQL(app)
 
 
 @app.route("/")
@@ -22,6 +37,21 @@ def login():
         password = request.form["password"]
         return render_template("dashboard.html")
     return render_template("login.html")
+
+
+@app.route("/contact")
+def create_contact():
+    if request.method == "POST":
+        print(request.form)
+        phoneNumber = request.form["phoneNumber"]
+        city = request.form["city"]
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            """INSERT INTO contact (phoneNumber,city) VALUES(%s,%s)""",
+            (phoneNumber, city),
+        )
+        mysql.connection.commit()
+        cursor.close()
 
 
 @app.route("/dashboard")
