33fa2fd18baaa47bbd3cf14d33fcf170b6bc27e0
rinto11804 <rinto11804@gmail.com>
2024-10-22 21:31:21 +0530

    made project,department, create and logic and register

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from dotenv import load_dotenv
 from flask_mysqldb import MySQL
 
@@ -24,36 +24,135 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    context = {}
+    cursor = mysql.connection.cursor()
+    if request.method == "POST":
+        print(request.form)
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        department = request.form["department"]
+        role = request.form["role"]
+        cursor.execute(
+            """INSERT INTO employee (name,email,password,departmentId) VALUES(%s,%s,%s,%s)""",
+            (name, email, password, department),
+        )
+        mysql.connection.commit()
+        cursor.close()
+        context["msg"] = "Your registed successfully"
+        return render_template("signup.html", context=context)
+
+    cursor.execute("SELECT id,name FROM department")
+    context["departments"] = list(cursor.fetchall())
+    print(context["departments"])
+    cursor.close()
+    return render_template("signup.html", context=context)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    context = {}
     if request.method == "POST":
         print(request.form)
         email = request.form["email"]
         password = request.form["password"]
-        return render_template("dashboard.html")
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            """SELECT * FROM employee WHERE email=%s AND password=%s""",
+            (email, password),
+        )
+        employee = cursor.fetchone()
+        cursor.close()
+        print(employee)
+        context["employee"] = employee
+        return redirect("dashboard")
     return render_template("login.html")
 
 
-@app.route("/contact")
+@app.route("/employee/contact")
 def create_contact():
+    cursor = mysql.connection.cursor()
     if request.method == "POST":
         print(request.form)
         phoneNumber = request.form["phoneNumber"]
         city = request.form["city"]
-        cursor = mysql.connection.cursor()
         cursor.execute(
             """INSERT INTO contact (phoneNumber,city) VALUES(%s,%s)""",
             (phoneNumber, city),
         )
         mysql.connection.commit()
-        cursor.close()
+    cursor.execute(
+        "SELECT e.name,e.email,c.phoneNumber,c.city FROM contact c JOIN employee e ON c.id = e.contactId"
+    )
+    contacts = cursor.fetchall()
+    cursor.close()
+    return render_template("contact.html", contacts=contacts)
 
 
-@app.route("/dashboard")
+# @app.route("/employee/profile")
+# def view_profile():
+#     cursor = mysql.connection.cursor()
+#     cursor.execute(
+#         "SELECT e.name,e.email,c.phoneNumber,c.city FROM contact c JOIN employee e ON c.id = e.contactId"
+#     )
+#     contacts = cursor.fetchall()
+#     cursor.close()
+#     return render_template("profile.html", employee=employees[0])
+
+
+@app.route("/departments", methods=["GET", "POST"])
+def departments_page():
+    cursor = mysql.connection.cursor()
+    if request.method == "POST":
+        name = (request.form.get("name"),)
+        description = (request.form.get("description"),)
+        cursor.execute(
+            """INSERT INTO department (name,description) VALUES(%s,%s)""",
+            (name, description),
+        )
+        mysql.connection.commit()
+        cursor.execute("SELECT name,description FROM department")
+        departments = cursor.fetchall()
+        print(departments)
+        return render_template("department.html", departments=departments)
+    cursor.execute("SELECT name,description FROM department")
+    departments = list(cursor.fetchall())
+    print(departments)
+    cursor.close()
+    return render_template("department.html", departments=departments)
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
-    return render_template("dashboard.html")
+    context = {}
+    cursor = mysql.connection.cursor()
+    if request.method == "POST":
+        # Extract form data
+        project_name = request.form.get("name")
+        description = request.form.get("description")
+        priority = request.form.get("priority")
+        deadline = request.form.get("deadline")
+        departmentId = request.form.get("department")
+
+        insert_query = """
+            INSERT INTO projects (name, description, priority, deadline, departmentId)
+            VALUES (%s, %s, %s, %s, %s);
+        """
+
+        print(departmentId)
+        cursor.execute(
+            insert_query, (project_name, description, priority, deadline, departmentId)
+        )
+        mysql.connection.commit()
+        return redirect(url_for("dashboard"))
+    cursor.execute("SELECT id, name, description, priority, deadline FROM projects;")
+    projects = list(cursor.fetchall())
+    context["projects"] = projects
+    cursor.execute("SELECT id,name FROM department")
+    departments = list(cursor.fetchall())
+    print(departments)
+    context["departments"] = departments
+    cursor.close()
+    return render_template("dashboard.html", context=context)
--- tables.sql
+++ tables.sql
@@ -1,30 +1,41 @@
-CREATE TABLE employee IF NOT EXISTS (
+CREATE TABLE contact(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    phoneNumber VARCHAR(20) NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     departmentId INT NOT NULL,
     contactId INT NOT NULL,
-    FOREIGN KEY(departmentId) REFERENCES department(id),
     FOREIGN KEY(contactId) REFERENCES contact(id)
-)
+);
 
-CREATE TABLE contact IF NOT EXISTS (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    phoneNumber VARCHAR(20) NOT NULL,
-    city VARCHAR(50) NOT NULL
-)
-
-CREATE TABLE department IF NOT EXISTS (
+CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     managerId INT NOT NULL,
     FOREIGN KEY(managerId) REFERENCES employee(id)
-)
+);
 
-CREATE TABLE project IF NOT EXISTS (
+ALTER TABLE employee ADD FOREIGN KEY(departmentId) REFERENCES department(id);
+ALTER TABLE contact ADD address VARCHAR(200);
+ALTER TABLE employee ADD password VARCHAR(20);
+ALTER TABLE employee ADD email VARCHAR(100) NOT NULL UNIQUE;
+alter table department add description VARCHAR(200);
+alter table department modify column managerId INT;
+alter table employee modify column contactId INT;
+
+CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    startDate DATE NOT NULL
-)
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    priority ENUM('low', 'medium', 'high') NOT NULL,
+    deadline DATE NOT NULL,
+    departmentId INT NOT NULL,
+    FOREIGN KEY(departmentId) REFERENCES department(id)
+);
 
 CREATE TABLE employee_project_link IF NOT EXISTS (
     id INT AUTO_INCREMENT PRIMARY KEY,
